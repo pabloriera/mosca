9bd4f554943cfaee5a6168e4e9c2a84f1f7e8600
carlo1112 <carlo1112@gmail.com>
2014-10-01 16:24:22 -0300

    osc_pb_win_1

--- osc_pb/src/ofxOsc_pb.h
+++ osc_pb/src/ofxOsc_pb.h
@@ -1,5 +1,14 @@
 #ifndef _OSC_PB
+
     #define _OSC_PB
+
+    #include <map>
+    #include <string>
+    #include "stdlib.h"
+    #include "ofxOsc.h"
+
+    using namespace std;
+
 
     class OSC_pb
     {
@@ -8,11 +17,17 @@
         class paquete
             {
                 public:
-                paquete(float _x, float _minX, _maxX, _minY, _maxY)
-                            {x = _x; minX = _minX, etc	}
+                paquete(float _x, float _minX, float _maxX, float _minY, float _maxY)
+                {
+                    x = _x;
+                    minX = _minX;
+                    maxX = _maxX;
+                    minY = _minY;
+                    maxY = _maxY;
+                }
 
                 float* x;
-            float minX, maxX, minY, maxY;
+                float minX, maxX, minY, maxY;
             };
 
         typedef map<string, paquete> map_receive;
